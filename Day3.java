6a49270adc514696260aa902d29f78b25372a62c
Cody O'Brien <65051473+CodyOBrien16@users.noreply.github.com>
2024-12-03 21:22:13 +0000

    day 3 complete

--- Day3.java
+++ Day3.java
@@ -23,8 +23,8 @@ public class Day3 {
         }
 
 
-        //String input = dataList.get(0);
-        String input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        String input = dataList.get(0);
+        //String input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
         String regex = "mul\\(\\d{1,3},\\d{1,3}\\)|don't\\(\\)|do\\(\\)";
 
@@ -38,10 +38,49 @@ public class Day3 {
             matches.add(matcher.group());
         }
 
-        for (String item : matches) {
-            System.out.println(item);
-        }
+        // for (String item : matches) {
+        //     System.out.println(item);
+        // }
         
+
+        String[][] StrNums = new String[matches.size()][2];
+
+        boolean active = true;
+        for (int i = 0; i < matches.size(); i++) {
+            if (matches.get(i).equals("don't()")) {
+                active = false;
+            }
+            else if (matches.get(i).equals("do()")) {
+                active = true;
+            } else if (matches.get(i) != "do()" && matches.get(i) != "don't()" && active == true) {
+                String temp = matches.get(i).substring(4,matches.get(i).length()-1);
+                StrNums[i] = temp.split(",");
+            }
+            
+        }
+        for (String[] item : StrNums) {
+            System.out.println(Arrays.toString(item));
+        }
+
+        int[] sumList = new int[matches.size()];
+        for (int i = 0; i<StrNums.length; i++) {
+            int multTotal = 1;
+            if (StrNums[i][0] != null) {
+                for (int j = 0; j<StrNums[i].length; j++) {
+                    multTotal *= Integer.parseInt(StrNums[i][j]);
+                }
+                sumList[i] = multTotal;
+            }
+            
+        }
+
+        int total = 0;
+        for (int num : sumList) {
+            total += num;
+        }
+        System.out.println(total);
+
+
 
         //Part 1
         // String[][] StrNums = new String[matches.size()][2];
