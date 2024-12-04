48ceeafe134bb92d45ca2d18f3f2ab1bc00e43bc
Cody O'Brien <65051473+CodyOBrien16@users.noreply.github.com>
2024-12-04 15:41:14 +0000

    added logic but not working

--- Day4.java
+++ Day4.java
@@ -10,7 +10,7 @@ public class Day4 {
         ArrayList<String> dataList = new ArrayList<String>();
 
         // try {
-        // File myObj = new File("input2.3.txt");
+        // File myObj = new File("input2.4.txt");
         // Scanner myReader = new Scanner(myObj);
         // while (myReader.hasNextLine()) {
         //     String data = myReader.nextLine();
@@ -43,57 +43,77 @@ public class Day4 {
         for (char[] item : inputList) {
             System.out.println(Arrays.toString(item));
         }
-
+        int total = 0;
         int length = inputList.length;
         int width = inputList[0].length;
         for (int i = 0; i < inputList.length; i++){
             for (int j = 0; j < inputList[i].length; j++) {
                 if (inputList[i][j] == 'X') {
                     //check if can look right
-                    if (j < width-4 && j > 0) {
-                        //add logic
+                    if (j < width-4 && j >= 0) {
+                        //System.out.println("right");
+                        if(inputList[i][j+1] == 'M' && inputList[i][j+2] == 'A' && inputList[i][j+3] == 'S') {
+                            total += 1;
+                        }
                     }
 
                     // check if can look left
                     if (j > 3 && j < width-1) {
-
+                        //System.out.println("left");
+                        if(inputList[i][j-1] == 'M' && inputList[i][j-2] == 'A' && inputList[i][j-3] == 'S') {
+                            total += 1;
+                        }
                     }
 
                     // check if can look up
                     if (i > 3 && i < length-1) {
-
+                        //System.out.println("up");
+                        if(inputList[i-1][j] == 'M' && inputList[i-2][j] == 'A' && inputList[i-3][j] == 'S') {
+                            total += 1;
+                        }
                     }
 
                     // check if can look down
-                    if (i < length-4 && i > 0) {
-
+                    if (i < length-4 && i >= 0) {
+                        //System.out.println("down");
+                        if(inputList[i+1][j] == 'M' && inputList[i+2][j] == 'A' && inputList[i+3][j] == 'S') {
+                            total += 1;
+                        }
                     }
 
                     // check if can look diagonal up/right
-                    if (j < width-4 && j > 0 && i > 3 && i < length-1) {
-
+                    if (j < width-4 && j >= 0 && i > 3 && i < length-1) {
+                        if(inputList[i-1][j+1] == 'M' && inputList[i-2][j+2] == 'A' && inputList[i-3][j+3] == 'S') {
+                            total += 1;
+                        }
                     }
 
 
                     // check if can look diagonal down/right
-                    if (i < length-4 && i > 0 && j < width-4 && j > 0) {
-
+                    if (i < length-4 && i >= 0 && j < width-4 && j >= 0) {
+                        if(inputList[i+1][j+1] == 'M' && inputList[i+2][j+2] == 'A' && inputList[i+3][j+3] == 'S') {
+                            total += 1;
+                        }
                     }
 
                     // check if can look diagonal down/left
-                    if (i < length-4 && i > 0 && j > 3 && j < width-1) {
-
+                    if (i < length-4 && i >= 0 && j > 3 && j < width-1) {
+                        if(inputList[i+1][j-1] == 'M' && inputList[i+2][j-2] == 'A' && inputList[i+3][j-3] == 'S') {
+                            total += 1;
+                        }
                     }
 
                     // check if can look diagonal up/left
                     if (i > 3 && i < length-1 && j > 3 && j < width-1) {
-
+                        if(inputList[i-1][j-1] == 'M' && inputList[i-2][j-2] == 'A' && inputList[i-3][j-3] == 'S') {
+                            total += 1;
+                        }
                     }
 
                 }
             }
         }
-
+        System.out.println(total);
         
     }
 }
