3e8bdbf075af9293495a893b3a52b051fa7834ae
Cody O'Brien <65051473+CodyOBrien16@users.noreply.github.com>
2024-12-06 16:16:56 +0000

    working for sample

--- Day5.java
+++ Day5.java
@@ -8,47 +8,47 @@ public class Day5 {
     public static void main(String[] args) {
         ArrayList<String> dataList = new ArrayList<String>();
 
-        // try {
-        // File myObj = new File("input2.4.txt");
-        // Scanner myReader = new Scanner(myObj);
-        // while (myReader.hasNextLine()) {
-        //     String data = myReader.nextLine();
-        //     dataList.add(data);
-        // }
-        // myReader.close();
-        // } catch (FileNotFoundException e) {
-        // System.out.println("An error occurred.");
-        // e.printStackTrace();
-        // }
+        try {
+        File myObj = new File("input2.5.txt");
+        Scanner myReader = new Scanner(myObj);
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            dataList.add(data);
+        }
+        myReader.close();
+        } catch (FileNotFoundException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+        }
 
-        dataList.add("47|53");
-        dataList.add("97|13");
-        dataList.add("97|61");
-        dataList.add("97|47");
-        dataList.add("75|29");
-        dataList.add("61|13");
-        dataList.add("75|53");
-        dataList.add("29|13");
-        dataList.add("97|29");
-        dataList.add("53|29");
-        dataList.add("61|53");
-        dataList.add("97|53");
-        dataList.add("61|29");
-        dataList.add("47|13");
-        dataList.add("75|47");
-        dataList.add("97|75");
-        dataList.add("47|61");
-        dataList.add("75|61");
-        dataList.add("47|29");
-        dataList.add("75|13");
-        dataList.add("53|13");
-        dataList.add(" ");
-        dataList.add("75,47,61,53,29");
-        dataList.add("97,61,53,29,13");
-        dataList.add("75,29,13");
-        dataList.add("75,97,47,61,53");
-        dataList.add("61,13,29");
-        dataList.add("97,13,75,29,47");
+        // dataList.add("47|53");
+        // dataList.add("97|13");
+        // dataList.add("97|61");
+        // dataList.add("97|47");
+        // dataList.add("75|29");
+        // dataList.add("61|13");
+        // dataList.add("75|53");
+        // dataList.add("29|13");
+        // dataList.add("97|29");
+        // dataList.add("53|29");
+        // dataList.add("61|53");
+        // dataList.add("97|53");
+        // dataList.add("61|29");
+        // dataList.add("47|13");
+        // dataList.add("75|47");
+        // dataList.add("97|75");
+        // dataList.add("47|61");
+        // dataList.add("75|61");
+        // dataList.add("47|29");
+        // dataList.add("75|13");
+        // dataList.add("53|13");
+        // dataList.add(" ");
+        // dataList.add("75,47,61,53,29");
+        // dataList.add("97,61,53,29,13");
+        // dataList.add("75,29,13");
+        // dataList.add("75,97,47,61,53");
+        // dataList.add("61,13,29");
+        // dataList.add("97,13,75,29,47");
 
 
         ArrayList<String> patternList = new ArrayList<>();
@@ -100,9 +100,11 @@ public class Day5 {
 
             //sort through patterns to find matches
             for (int j = 0; j < patternList2.length; j++) {
-                if (currentList.contains(patternList2[i][0]) && currentList.contains(patternList2[i][1])) {
+                if (currentList.contains(patternList2[j][0]) && currentList.contains(patternList2[j][1])) {
                     //add logic to check here
-                    if (currentList.indexOf(patternList2[i][0]) > currentList.indexOf(patternList2[i][1])) {
+                    int leftIndex = currentList.indexOf(patternList2[j][0]);
+                    int rightIndex = currentList.indexOf(patternList2[j][1]);
+                    if (leftIndex < rightIndex) {
                         correctOrder = true;
                     } else {
                         correctOrder = false;
@@ -114,10 +116,22 @@ public class Day5 {
                 finalList.add(currentList);
             }
         }
-
-        for (String item : finalList) {
-            System.out.println(item);
+        int[] finalTotalList = new int[finalList.size()];
+        // for (String item : finalList) {
+        for (int i = 0; i < finalList.size(); i++) {
+            String[] temp = finalList.get(i).split(",");
+            if (temp.length % 2 == 0) { //even
+                finalTotalList[i] = Integer.parseInt(temp[((temp.length) / 2)-1]);
+            } else if (temp.length % 2 == 1) {
+                int temp3 = Math.round((temp.length) / 2);
+                finalTotalList[i] = Integer.parseInt(temp[temp3]);
+            }
         }
+        int theFinalTotal = 0;
+        for (int item : finalTotalList) {
+            theFinalTotal += item;
+        }
+        System.out.println(theFinalTotal);
 
     }
 }
